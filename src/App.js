49ce07e47075b763e1efa21084b73c32b5f41756
Roonie <roonierue@gmail.com>
2021-11-03 19:26:26 +0200

    applied useState on app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, {  Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './Components/layout/NavBar';
 import Users from './Components/Users/Users';
@@ -9,15 +9,13 @@ import About from './Components/pages/About';
 import User from './Components/Users/User';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null
-    
-  }
+const App = () => {
+  const [users, setUsers] = useState ([]);
+  const [user, setUser] = useState ({});
+  const [repos, setRepos] = useState ([]);
+  const [loading, setLoading] = useState (false);
+  const [alert, setAlert] = useState (null);
+
 
   //async componentDidMount () {
 
@@ -29,53 +27,58 @@ class App extends Component {
   //}
 
   //search github users
-  searchUsers = async text => {
-    this.setState({loading: true});
+  const searchUsers = async text => {
+    setLoading(true);
 
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     
-    this.setState({users: res.data.items, loading: false});
+    setUsers(res.data.items);
+    setLoading(false);
   }
 
   //get single git hub user
 
-  getUser = async (username) => {
-    this.setState({loading: false});
+  const getUser = async (username) => {
+    setLoading(true);
+
 
     const res = await axios.get(
     `https://api.github.com/search/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
   
-    this.setState({user: res.data, loading: false});
+    setUser(res.data);
+    setLoading(false);
 };
 
 // get users repos
 
-getUserRepos = async (username) => {
-  this.setState({loading: false});
+  const getUserRepos = async (username) => {
+    setLoading(true);
 
   const res = await axios.get(
   `https://api.github.com/search/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
-  this.setState({repos: res.data, loading: false});
+  
+    setRepos(res.data);
+    setLoading(false);
 };
 
   //clear users from state
 
-  clearUsers = () => this.setState({ users: [], loading: false});
-
-  //set alert
-  setAlert=(msg, type) => {
-    this.setState ({ alert: { msg, type} });
-
-    setTimeout(() => this.setState({ alert: null}), 3000)
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   };
 
-  render() {
-    const { users, loading, alert, user, repos} = this.state;
+  //set alert
+  const showAlert = (msg, type) => {
+    setAlert ({ msg, type} );
+    setTimeout(() => setAlert(null), 3000)
+  };
+
     
     return (
       <Router>
@@ -87,10 +90,10 @@ getUserRepos = async (username) => {
             <Route exact path ='/' render= { props => (
               <Fragment>
                   <Search 
-                    searchUsers ={this.searchUsers} 
-                    clearUsers ={this.clearUsers} 
+                    searchUsers ={searchUsers} 
+                    clearUsers ={clearUsers} 
                     showClear= {users.length > 0 ? true: false}
-                    setAlert={this.setAlert}
+                    setAlert={showAlert}
                   />
                   <Users loading={loading} users={users}/>
               </Fragment>
@@ -99,8 +102,8 @@ getUserRepos = async (username) => {
             <Route exact path = '/user/:login' render={props => (
               <User 
               {...props} 
-              getUser={this.getUser} 
-              getUserRepos={this.getUserRepos} 
+              getUser={getUser} 
+              getUserRepos={getUserRepos} 
               user={user}
               repos={repos}
               loading={loading}
@@ -114,7 +117,6 @@ getUserRepos = async (username) => {
       </Router>
     );
   }
-}
 
 
 
